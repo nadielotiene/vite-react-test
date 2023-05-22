2cfac63962c9358b2c347385897deeb2edb5dbf5
Kenneth Velazquez <kenny.arcelay@gmail.com>
2023-05-22 15:37:45 -0500

    png fix

--- src/iconData.js
+++ src/iconData.js
@@ -2,71 +2,71 @@ export default [
     {
         id: "1",
         alt: "Rooms",
-        source: "/rooms.png"
+        source: "./rooms.png"
     },
     {
         id: "2",
         alt: "Cabins",
-        source: "/cabins.png"
+        source: "./cabins.png"
     },
     {
         id: "3",
         alt: "Beachfront",
-        source: "/beachfront.png"
+        source: "./beachfront.png"
     },
     {
         id: "4",
         alt: "Treehouses",
-        source: "/treehouses.png"
+        source: "./treehouses.png"
     },
     {
         id: "5",
         alt: "Lakefront",
-        source: "/lakefront.png"
+        source: "./lakefront.png"
     },
     {
         id: "6",
         alt: "OMG!",
-        source: "/omg.png"
+        source: "./omg.png"
     },
     {
         id: "7",
         alt: "Mansions",
-        source: "/mansions.png"
+        source: "./mansions.png"
     },
     {
         id: "8",
         alt: "Trending",
-        source: "/trending.png"
+        source: "./trending.png"
     },
     {
         id: "9",
         alt: "Pools",
-        source: "/pools.png"
+        source: "./pools.png"
     },
     {
         id: "10",
         alt: "Countryside",
-        source: "/countryside.png"
+        source: "./countryside.png"
     },
     {
         id: "11",
         alt: "Boats",
-        source: "/boats.png"
+        source: "./boats.png"
     },
     {
         id: "12",
         alt: "Camping",
-        source: "/camping.png"
+        source: "./camping.png"
     },
     {
         id: "13",
         alt: "Castles",
-        source: "/castles.png"
+        source: "./castles.png"
     },
     {
         id: "14",
         alt: "Igloos",
-        source: "/igloos.png"
+        source: "./igloos.png"
     },
 ]
